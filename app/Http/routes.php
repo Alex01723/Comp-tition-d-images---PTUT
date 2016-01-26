f1e6845eb14040c4f270e9605c0cf655f6c837fc
Alex <alexis.picot@etu.univ-lyon1.fr>
2016-01-26 11:20:54 +0100

    v0.5 / Authentification fonctionnelle à 100 %

---
+++ app/Http/routes.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('home', '\Bestmomo\Scafold\Http\Controllers\HomeController@index');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+
+
+
+Menu::make('MyNavBar', function($menu){
+    $menu->add('Accueil', '/:8888');
+    $menu->add('Compétitions en cours','competitions');
+    $menu->add('Résultats des compétitions', 'resultats');
+    $menu->add('Jugements',  'jugements');
+});
+
