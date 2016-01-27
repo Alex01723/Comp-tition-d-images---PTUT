dcc42cab274f7e86c33ee3e97709e1fce9ad17d5
Alex <alexis.picot@etu.univ-lyon1.fr>
2016-01-27 12:02:36 +0100

    v0.7.3 / Mise à jour des vues

--- resources/views/Campagne.blade.php
+++ resources/views/Campagne.blade.php
@@ -1,5 +1,15 @@
 @extends('app')
 
 @section('content')
-    {{ "Je suis la campagne " . $campagne->id_campagne }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ "Détail de la campagne " . $campagne->nom_campagne }}</div>
+                    <a href="{{ $campagne->id_campagne .'/submit' }}">Lien</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
--- resources/views/campagnes.blade.php
+++ resources/views/campagnes.blade.php
@@ -1,17 +1,27 @@
 @extends('app')
 
 @section('content')
-    <div>
-        <ul>
-        @foreach ($campagnes as $campagne)
-            <li>
-                <a href="{{ "campagne/" . $campagne->id_campagne }}">{{ $campagne->nom_campagne }}</a>
-                <ul>
-                    <li>{{ $campagne->description_campagne }}</li>
-                    <li>{{ $campagne->date_fin }}</li>
-                </ul>
-            </li>
-        @endforeach
-        <ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Liste des campagnes</div>
+                    <div>
+                        <ul>
+                            @foreach ($campagnes as $campagne)
+                                <li>
+                                    <a href="{{ "campagne/" . $campagne->id_campagne }}">{{ $campagne->nom_campagne }}</a>
+                                    <ul>
+                                        <li>{{ $campagne->description_campagne }}</li>
+                                        <li>{{ $campagne->date_fin }}</li>
+                                    </ul>
+                                </li>
+                            @endforeach
+                            <ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 @endsection
