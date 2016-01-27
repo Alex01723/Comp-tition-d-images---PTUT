21c7a344807e70b99a43090ff6de56200772e36d
Alex <alexis.picot@etu.univ-lyon1.fr>
2016-01-27 18:08:43 +0100

    v0.7.6 / Légère amélioration de l'affichage d'une campagne

--- resources/views/Campagne.blade.php
+++ resources/views/Campagne.blade.php
@@ -1,15 +1,18 @@
 @extends('app')
 
 @section('content')
+
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
+                <div class="panel panel-default" >
                     <div class="panel-heading">{{ "Détail de la campagne " . $campagne->nom_campagne }}</div>
-                    <a href="{{ $campagne->id_campagne .'/submit' }}">Lien</a>
-                    @foreach ($images as $image)
-                        {{ var_dump($image->id_campagne) }}
-                    @endforeach
+                    <a href="{{ $campagne->id_campagne .'/submit' }}" style="display: block; width: auto;">Lien</a>
+                    <div style="text-align: left;">
+                        @foreach ($images as $image)
+                            <div style='background-image:url("/uploads/{{$image->lien_image}}");' class="miniaturesImages">gdfgd<br></div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
